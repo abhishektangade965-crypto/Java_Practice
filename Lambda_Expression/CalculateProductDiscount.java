928913870e64d50330f13ea87e928d53525d0d99
Abhishek Tangade <abhishektangade965@gmail.com>
2025-12-23 21:56:05 +0530

    Calucate

---
+++ Lambda_Expression/CalculateProductDiscount.java
@@ -0,0 +1,55 @@
+package Lambda_Expression;
+
+import java.util.function.Function;
+
+record Product (Integer id, String name, Double Price)
+{
+	
+}
+
+
+
+
+public class CalculateProductDiscount
+{
+
+	public static void main(String[] args) 
+	{
+		Integer id= Integer.parseInt(IO.readln("Enter Product Id :"));
+		String name =IO.readln("Enter Product Name :");
+		Double Price= Double.parseDouble(IO.readln("Enter Product Price :"));
+	
+		Product p = new Product(id, name, Price);
+		
+	Function<Product, Double> P = P1 ->  
+	{
+		if(P1.Price()>=5000)
+		{
+			return p.Price()-p.Price()*0.10;
+			
+		}
+		else if (P1.Price()<5000)
+		{
+			return p.Price()-p.Price()*0.05;
+		}
+		 return Price;
+	};
+	
+	IO.println("Final Price is : "+P.apply(p));
+	
+	
+	
+	
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
